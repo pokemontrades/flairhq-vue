5c1271c68e83810d241960758c1e59825ede9799
Paul Hurlbut <paulhurlbut@wi.rr.com>
2026-05-24 15:03:30 -0500

    session config for tests

--- flairhq-api/src/main/java/org/ptrades/flairhq/config/SessionConfig.java
+++ flairhq-api/src/main/java/org/ptrades/flairhq/config/SessionConfig.java
@@ -1,8 +1,10 @@
 package org.ptrades.flairhq.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
 
+@Profile("!test")
 @Configuration
 @EnableMongoHttpSession(maxInactiveIntervalInSeconds = 300)
 public class SessionConfig {
--- flairhq-api/src/test/java/org/ptrades/flairhq/FlairhqApplicationTests.java
+++ flairhq-api/src/test/java/org/ptrades/flairhq/FlairhqApplicationTests.java
@@ -1,13 +1,50 @@
 package org.ptrades.flairhq;
 
 import org.junit.jupiter.api.Test;
+import org.ptrades.flairhq.repository.ApplicationRepository;
+import org.ptrades.flairhq.repository.CommentRepository;
+import org.ptrades.flairhq.repository.EventRepository;
+import org.ptrades.flairhq.repository.FlairRepository;
+import org.ptrades.flairhq.repository.GameRepository;
+import org.ptrades.flairhq.repository.ModmailRepository;
+import org.ptrades.flairhq.repository.ModnoteRepository;
+import org.ptrades.flairhq.repository.ReferenceRepository;
+import org.ptrades.flairhq.repository.RejectionReasonRepository;
+import org.ptrades.flairhq.repository.UserRepository;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=" +
+        "org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.session.SessionAutoConfiguration"
+})
+@ActiveProfiles("test")
 class FlairhqApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockitoBean MongoTemplate mongoTemplate;
+
+    @MockitoBean FindByIndexNameSessionRepository<? extends Session> sessionRepository;
+
+    @MockitoBean ApplicationRepository applicationRepository;
+    @MockitoBean CommentRepository commentRepository;
+    @MockitoBean EventRepository eventRepository;
+    @MockitoBean FlairRepository flairRepository;
+    @MockitoBean GameRepository gameRepository;
+    @MockitoBean ModmailRepository modmailRepository;
+    @MockitoBean ModnoteRepository modnoteRepository;
+    @MockitoBean ReferenceRepository referenceRepository;
+    @MockitoBean RejectionReasonRepository rejectionReasonRepository;
+    @MockitoBean UserRepository userRepository;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
